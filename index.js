37965358ae2208ce5cf9b074976f455070c83d77
ardhendu_init_ <ardhendujj002@gmail.com>
2023-06-27 16:25:07 +0530

    Backend of e-commerce app has been added

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+
+const { json } = require("express");
+app.use(json());
+app.use(cors());
+
+const mongoUrl = process.env.MONGO_URL;
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("Db connected Successfully");
+  })
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("app is running ");
+});
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/orders", orderRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server is running on port 5000");
+});
---
+++ routes/auth.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+//Register
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//Login
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      res.status(401).json("wrong username");
+    }
+    const hashPass = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.SECRET_KEY
+    );
+    const originalPass = hashPass.toString(CryptoJS.enc.Utf8);
+    const givenPassword = req.body.password;
+    if (originalPass != givenPassword) {
+      res.status(401).json("Wrong Password");
+    }
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JSON_SECRET_KEY,
+      { expiresIn: "2d" }
+    );
+    const { password, ...others } = user._doc;
+    res.status(200).json({ ...others, accessToken });
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
