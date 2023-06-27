2e01ec9b2b437f72faf770ce3f02b71a30e8f2d0
ardhendu_init_ <ardhendujj002@gmail.com>
2023-06-27 17:13:23 +0530

    minor chnages

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.use("/api/products", productRoute);
 app.use("/api/cart", cartRoute);
 app.use("/api/orders", orderRoute);
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server is running on port 5000");
-});
+// app.listen(process.env.PORT || 5000, () => {
+//   console.log("Server is running on port 5000");
+// });
